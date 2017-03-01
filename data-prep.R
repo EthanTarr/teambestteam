7135442aaa66961b13ba62413cc41cb00ab37764
Nico Malig <nicomalig@gmail.com>
2017-03-01 10:36:10 -0800

    Moar stuff

--- data-prep.R
+++ data-prep.R
@@ -7,3 +7,10 @@
 library(dplyr)
 library(xlsx)
 library(tidyr)
+
+# Uncomment when you need this run, the xlsx package takes FOREVER to read stuff...
+# data.raw.illinois <- read.csv("data/IL-ab-demo.csv")
+# data.raw.un <- read.xlsx("data/UNPD2016.xlsx", sheetName = 'DATA')
+
+data.raw.un.colnames <- data.raw.un[3,]
+data.raw.un.colnames2 <- data.raw.un[4,]
