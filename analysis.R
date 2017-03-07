12cfe01a958f492144b27d2a87203d038784f474
Spencer Pease <spencer.p.816@gmail.com>
2017-03-07 02:12:14 -0800

    Format code

--- analysis.R
+++ analysis.R
@@ -20,9 +20,9 @@ natl.outcome.measures <- read_csv('Data/natl.outcome.measures.csv') %>%
     mutate(Year = make_date(Year))
 
 
-################
-### Analysis ###
-################
+###################
+### Format data ###
+###################
 
 ## Sum abortion data ##
 
@@ -42,12 +42,28 @@ all.totals <- bind_rows(residence.totals, service.totals) %>%
     arrange(Year)
 
 
-## Summary plots ##
+## Create long data ##
 
 # NATL data over time
 natl.long <- natl.outcome.measures %>%
     gather('Metric', 'Rate', 2:5)
 
+# Abortions by state of residence over time
+ab.res.long <- abortions.by.residence %>%
+    gather('Year', 'value', -1)
+
+# Abortions by state of service over time
+ab.srv.long <- abortions.by.service %>%
+    gather('Year', 'value', -1)
+
+
+#####################
+### Create graphs ###
+#####################
+
+## Summary plots ##
+
+# NATL data over time
 g.natl.summary <- ggplot(data = natl.long,
                          aes(x = Year,
                              y = Rate,
@@ -58,8 +74,6 @@ g.natl.summary <- ggplot(data = natl.long,
     labs(title = 'NATL Outcome Metrics over Time',
          x = 'Year',
          y = 'Rate (per 10,000 people)')
-
-# g.natl.summary
 
 
 # Abortion totals over time
@@ -75,16 +89,11 @@ g.ab.tot.summary <- ggplot(data = all.totals,
          x = 'Year',
          y = 'Number')
 
-# g.ab.tot.summary
-
 
 # Abortions by state of residence over time
-ab.res.long <- abortions.by.residence %>%
-    gather('Year', 'value', -1)
-
 g.ab.res.summary <- ggplot(data = ab.res.long,
                            aes(x = Year,
-                               y = log(value),
+                               y = value,
                                color = State,
                                group = State)) +
     geom_line(size = 1) +
@@ -94,13 +103,8 @@ g.ab.res.summary <- ggplot(data = ab.res.long,
          x = 'Year',
          y = 'Number')
 
-# g.ab.res.summary
-
 
 # Abortions by state of service over time
-ab.srv.long <- abortions.by.service %>%
-    gather('Year', 'value', -1)
-
 g.ab.srv.summary <- ggplot(data = ab.srv.long,
                            aes(x = Year,
                                y = value,
@@ -112,6 +116,4 @@ g.ab.srv.summary <- ggplot(data = ab.srv.long,
          subtitle = 'For state of service; grouped by state',
          x = 'Year',
          y = 'Number')
-
-# g.ab.srv.summary
 
