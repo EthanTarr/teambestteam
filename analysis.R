43e861bbf51cf9a26c740d7373383214013a06c2
Spencer Pease <spencer.p.816@gmail.com>
2017-03-06 21:43:35 -0800

    Load data for analysis

--- analysis.R
+++ analysis.R
@@ -3,9 +3,13 @@
 
 # Needed libraries
 library(dplyr)
+library(readr)
 
-# Source files
-source('data-prep.R')
+
+# Read in data
+abortions.by.residence <- read_csv('Data/abortion.by.residence.csv')
+abortions.by.service <- read_csv('Data/abortion.by.service.csv')
+natl.outcome.measures <- read_csv('Data/natl.outcome.measures.csv')
 
 
 ################
