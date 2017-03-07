3622be1e28738c534fc5d24ed4985ce32518fa93
Spencer Pease <spencer.p.816@gmail.com>
2017-03-06 23:24:33 -0800

    Sum abortion data for residence and state

--- analysis.R
+++ analysis.R
@@ -4,6 +4,8 @@
 # Needed libraries
 library(dplyr)
 library(readr)
+library(tidyr)
+library(tibble)
 
 
 # Read in data
@@ -15,4 +17,16 @@ natl.outcome.measures <- read_csv('Data/natl.outcome.measures.csv')
 ################
 ### Analysis ###
 ################
+
+## Sum abortion data ##
+
+residence.totals <- colSums(abortions.by.residence[, -1], na.rm = T) %>%
+    as_tibble() %>%
+    rownames_to_column(var = 'Year') %>%
+    mutate(Year = as.numeric(Year))
+
+service.totals <- colSums(abortions.by.service[, -1], na.rm = T) %>%
+    as_tibble() %>%
+    rownames_to_column(var = 'Year') %>%
+    mutate(Year = as.numeric(Year))
 
