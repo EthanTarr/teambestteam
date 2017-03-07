a602daafb86f879f24e3bc8097f679f818979086
Spencer Pease <spencer.p.816@gmail.com>
2017-03-06 23:53:31 -0800

    Change years from numeric to date

--- analysis.R
+++ analysis.R
@@ -2,16 +2,21 @@
 #
 
 # Needed libraries
-library(dplyr)
 library(readr)
 library(tidyr)
+library(lubridate)
 library(tibble)
+library(dplyr)
 
 
 # Read in data
 abortions.by.residence <- read_csv('Data/abortion.by.residence.csv')
 abortions.by.service <- read_csv('Data/abortion.by.service.csv')
-natl.outcome.measures <- read_csv('Data/natl.outcome.measures.csv')
+natl.outcome.measures <- read_csv('Data/natl.outcome.measures.csv') %>%
+    select(year,
+           (1:4)) %>%
+    rename(Year = year) %>%
+    mutate(Year = make_date(Year))
 
 
 ################
@@ -23,10 +28,9 @@ natl.outcome.measures <- read_csv('Data/natl.outcome.measures.csv')
 residence.totals <- colSums(abortions.by.residence[, -1], na.rm = T) %>%
     as_tibble() %>%
     rownames_to_column(var = 'Year') %>%
-    mutate(Year = as.numeric(Year))
+    mutate(Year = make_date(Year))
 
 service.totals <- colSums(abortions.by.service[, -1], na.rm = T) %>%
     as_tibble() %>%
     rownames_to_column(var = 'Year') %>%
-    mutate(Year = as.numeric(Year))
-
+    mutate(Year = make_date(Year))
